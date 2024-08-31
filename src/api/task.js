e5d829aab75fe01f82a8dfdf37609a9b73355efc
facundotm <tmfacundo@gmail.com>
2024-08-31 15:04:22 -0300

    Fixed cookies problem.

--- src/api/task.js
+++ src/api/task.js
@@ -1,25 +1,8 @@
 import axios from "./axios";
-import Cookies from "js-cookie";
 
-const obtenerToken = () => {
-  const cookies = Cookies.get();
-  console.log("Empecé a ejecutar obtenerToken");
-  let config = {
-    headers: {
-      Authorization: `Bearer ${cookies.token}`,
-    },
-  };
-  console.log(`Las cookies son: ${cookies.token}`);
-  return config;
-};
-
-export const getTasksRequest = () => axios.get("/turnos", obtenerToken());
-export const getTaskRequest = (id) =>
-  axios.get(`/turnos/${id}`, obtenerToken());
-export const createTaskRequest = (task) =>
-  axios.post("/turnos", task, obtenerToken());
-export const updateTaskRequest = (id, task) =>
-  axios.put(`/turnos/${id}`, task, obtenerToken());
-export const deleteTaskRequest = (id) =>
-  axios.delete(`/turnos/${id}`, obtenerToken());
-export const getAdmTurnosRequest = () => axios.get("/admTask", obtenerToken());
+export const getTasksRequest = () => axios.get("/turnos");
+export const getTaskRequest = (id) => axios.get(`/turnos/${id}`);
+export const createTaskRequest = (task) => axios.post("/turnos", task);
+export const updateTaskRequest = (id, task) => axios.put(`/turnos/${id}`, task);
+export const deleteTaskRequest = (id) => axios.delete(`/turnos/${id}`);
+export const getAdmTurnosRequest = () => axios.get("/admTask");
